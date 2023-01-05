c31c7656ad0b424b2631773113de046d3275a67c
Heero <techheero@gmail.com>
2023-01-04 23:30:39 -0600

    Ayo whats all that movement back there?

--- leaderboard.py
+++ leaderboard.py
@@ -1,5 +1,5 @@
 #pull everything 
-from twitchio.ext import commands
+from twitchio.ext import commands, routines
 from clientshit import access_token
 
 class Bot(commands.Bot):
@@ -21,6 +21,10 @@ class Bot(commands.Bot):
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
+
+        #this is how the routine starts
+        self.send_leaderboard.start()
+        
 
 
     async def event_message(self, message):
@@ -68,12 +72,7 @@ class Bot(commands.Bot):
             print("🌟")
         else:
             print("🌕")
-
-        #this will sort and keep to a top 10
-        sorted_points_by_chatter = dict(sorted(self.points_by_chatter.items(), key=lambda x: x[1], reverse=True))
-        top_ten = dict(list(sorted_points_by_chatter.items())[:10])
-        print(top_ten)
- 
+        
 
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
@@ -89,6 +88,31 @@ class Bot(commands.Bot):
         # Sending a reply back to the channel is easy... Below is an example.
         await ctx.send(f'Hello {ctx.author.name}!')
 
+    '''
+    Bot account need to be mod to work
+    PARAMETERS
+    seconds: The seconds to wait before the next iteration of the routine.
+    minutes: The minutes to wait before the next iteration of the routine.
+    hours: The hours to wait before the next iteration of the routine.
+    time: A specific time to run this routine at. If a naive datetime is passed, your system local time will be used.
+    iterations: The amount of iterations to run this routine before stopping. If set to None or 0, the routine will run indefinitely.
+    wait_first: Whether to wait the specified time before running the first iteration. This has no effect when the time argument is used. Defaults to False.'''
+
+    @routines.routine(seconds=10.0, iterations=5)
+    async def send_leaderboard(self):
+        sorted_points_chatter = dict(sorted(self.points_by_chatter.item(), key=lambda x: x[1], reverse=True)
+        )
+        top_three = dict(list(sorted_points_chatter.items())[:3])
+
+        Ranking_message = ""
+        for y, (message.author.name,)
+
+        '''This will send message to specified channel every 10 seconds for 5 times, 
+           remove iteration as it will stop after 5 times
+           you can send the updated leaderboard here or print it out to console periodically
+        ''' 
+        
+        await self.get_channel("codingwithstrangers").send("Test")
 
 bot = Bot()
 bot.run()
