40aea2104d7c44d12e762ef86b5b6e0bd82798b6
VIGNESH KUMAR <45727291+VICTORVICKIE@users.noreply.github.com>
2023-01-12 09:02:41 +0530

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,32 +1,30 @@
-from operator import length_hint
 import random
-from twitchio.ext import commands, routines
+from operator import length_hint
+
 from clientshit import access_token
+from twitchio.ext import commands, routines
+
 
 class Bot(commands.Bot):
     points_by_chatter = {}
-    
-
 
     def __init__(self):
         # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
         # prefix can be a callable, which returns a list of strings or a string...
         # initial_channels can also be a callable which returns a list of strings...
-        
-        super().__init__(token= access_token , prefix='?', initial_channels=['codingwithstrangers'],
-            nick = "Perfect_Stranger")
+
+        super().__init__(token=access_token, prefix='?', initial_channels=['codingwithstrangers'],
+                         nick="Perfect_Stranger")
 
     async def event_ready(self):
-       
+
         # Notify us when everything is ready!
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
 
-        #this is how the routine starts
+        # this is how the routine starts
         self.send_leaderboard.start()
-        
-
 
     async def event_message(self, message):
         # Messages with echo set to True are messages sent by the bot...
@@ -37,43 +35,38 @@ class Bot(commands.Bot):
         # Print the contents of our message to console...
         print(message.content.encode("utf-8"))
         print(message.author.name)
-             
-    
-        
 
-        #this will add theuser to dic and count 
+        # this will add theuser to dic and count
         if message.author.name not in self.points_by_chatter.keys():
-            self.points_by_chatter[message.author.name]=0
-            
+            self.points_by_chatter[message.author.name] = 0
+
         else:
             self.points_by_chatter[message.author.name] += 1
-        
-        #minus point from user for infractions
+
+        # minus point from user for infractions
         # wight one bad word they wont get any point.
 
         for i in message.content.split():
             bad_words = ["strainbreh", "fun", ]
-            if i.lower() in bad_words:   
+            if i.lower() in bad_words:
                 if len(message.content) == 1:
-                    self.points_by_chatter[message.author.name] -= 0.5  
+                    self.points_by_chatter[message.author.name] -= 0.5
                 else:
                     self.points_by_chatter[message.author.name] -= 1.5
-             
-        
 
-         #check if user is subscriber or not
-        #if they use a channel emote "channel emote and are subbed" they are getting .5 point
-        subbed_chatters = ["coding32Thinkmybrother", "coding32Trunks", "coding32Whatmybrother", "coding32Zemi", "coding32Goten"]
+         # check if user is subscriber or not
+        # if they use a channel emote "channel emote and are subbed" they are getting .5 point
+        subbed_chatters = ["coding32Thinkmybrother", "coding32Trunks",
+                           "coding32Whatmybrother", "coding32Zemi", "coding32Goten"]
         for x in message.content.split():
             if x in subbed_chatters and message.author.is_subscriber == 1:
                 self.points_by_chatter[message.author.name] += .5
-                break    
+                break
         print(self.points_by_chatter)
         if message.author.is_subscriber:
             print("🌟")
         else:
             print("🌘")
-        
 
         # Since we have commands and are overriding the default `event_message`
         # We must let the bot know we want to handle and invoke our commands...
@@ -99,48 +92,58 @@ class Bot(commands.Bot):
     iterations: The amount of iterations to run this routine before stopping. If set to None or 0, the routine will run indefinitely.
     wait_first: Whether to wait the specified time before running the first iteration. This has no effect when the time argument is used. Defaults to False.'''
 
-    @routines.routine(seconds=30.0, iterations=5)
-    async def send_leaderboard(self):
-        sorted_points_chatter = dict(sorted(self.points_by_chatter.items(), key=lambda x: x[1], reverse=True)
-        )
-        top_three = dict(list(sorted_points_chatter.items())[:3])
+    async def Leaderboard_snap(self, sorted_chatter):
+        top_three = dict(list(sorted_chatter.items())[:3])
         # all_chatters = sorted_points_chatter
         # # Pick a random user and point from the points_by_chatter dictionary
         # random_user, random_points = random.choice(list(self.points_by_chatter.items()))
 
         Ranking_message = ""
-        for y, (name,points) in enumerate(top_three.items()):
+        for y, (name, points) in enumerate(top_three.items()):
             if y == 0:
-                Ranking_message+= f"***1st Place: {name} With {points} points is Leading The way coding32Zemi***\n"
+                Ranking_message += f"***1st Place: {name} With {points} points is Leading The way coding32Zemi***\n"
             elif y == 1:
-                Ranking_message+= f"2nd Place: {name} With {points} points is Not Far Behind coding32Valor***\n"
+                Ranking_message += f"2nd Place: {name} With {points} points is Not Far Behind coding32Valor***\n"
             elif y == 2:
-                Ranking_message+= f"3rd Place: {name} With {points} points Will Not Be Ignored coding32Key***\n"
+                Ranking_message += f"3rd Place: {name} With {points} points Will Not Be Ignored coding32Key***\n"
 
         '''This will send message to specified channel every 10 seconds for 5 times, 
            remove iteration as it will stop after 5 times
            you can send the updated leaderboard here or print it out to console periodically
-        ''' 
-        Ranking_message+= await self.random_chatter(sorted_points_chatter)
-        
+        '''
+        Ranking_message += await self.random_chatter(sorted_chatter)
+
+    @routines.routine(seconds=30.0, iterations=5)
+    async def send_leaderboard(self):
+        sorted_points_chatter = dict(sorted(self.points_by_chatter.items(), key=lambda x: x[1], reverse=True)
+                                     )
+
+        Ranking_message = await self.Leaderboard_snap(sorted_points_chatter)
 
         await self.get_channel("codingwithstrangers").send(Ranking_message)
 
-    async def random_chatter (self, all_chatters):
-        #we need all members after the 3rd member
-        #We need error checking to confirm our dict has at least 4 members
-        #If we have less than 4 members we will return a custom string to show this
-        if len(all_chatters)> 3:
+    @commands.command()
+    async def leaderboard(self, ctx: commands.Context):
+        sorted_points_chatter = dict(sorted(self.points_by_chatter.items(), key=lambda x: x[1], reverse=True)
+                                     )
+
+        Ranking_message = await self.Leaderboard_snap(sorted_points_chatter)
+
+        await ctx.send(Ranking_message)
+
+    async def random_chatter(self, all_chatters):
+        # we need all members after the 3rd member
+        # We need error checking to confirm our dict has at least 4 members
+        # If we have less than 4 members we will return a custom string to show this
+        if len(all_chatters) > 3:
             remaining_members = list(all_chatters.keys())[3:]
-            
-            return f"{random.choice(remaining_members)}, Is in the back but we see you"       
+
+            return f"{random.choice(remaining_members)}, Is in the back but we see you"
         else:
             return "coding32Thinkmybrother Come on Stranger I know you ain't Stalking and not Talking \n"
 
-               
-      
+
 # {'machinegun21k': 14, 'victor_vickie': 12.0, 'djwagner_gda': 7, 'grimybadger': 3, 'codingwithstrangers': 2, 'nightbot': 0}
-       
 bot = Bot()
 bot.run()
 # bot.run() is blocking and will stop execution of any below code here until stopped or closed.
